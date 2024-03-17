7e052c3c7c46365e81a2fdd2f9989030e81219a6
Craig Eaton <craigea92@gmail.com>
2024-03-17 11:32:40 +0000

    working with strings in csharp

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// Symbols
+
 string firstFriend = "Maria";
 string secondFriend = "Sage";
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+Console.WriteLine(firstFriend + secondFriend);
+
+// String Interpolation
 
 Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
-Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters."); 
+
+// Working with Strings (Trim)
+
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
