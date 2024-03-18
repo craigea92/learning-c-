3b093b5524c7bce094dac588535c99ebc67bd53a
Craig Eaton <craigea92@gmail.com>
2024-03-18 11:32:40 +0000

    replace with strings in csharp

--- Program.cs
+++ Program.cs
@@ -25,3 +25,13 @@ Console.WriteLine($"[{trimmedGreeting}]");
 
 trimmedGreeting = greeting.Trim();
 Console.WriteLine($"[{trimmedGreeting}]");
+
+// Working with Strings (Replace)
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
